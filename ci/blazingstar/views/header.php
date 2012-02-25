55f59b656359c875e2dd21b507ba9406c428e6b0
syed shah <syeed4@gmail.com>
2012-02-25 13:18:37 +0000

    changed top header

--- ci/blazingstar/views/header.php
+++ ci/blazingstar/views/header.php
@@ -8,13 +8,13 @@
     <script type="text/javascript" src="media/js/jqcloud-0.2.10.js"></script>
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
 
-   <!-- <link rel="stylesheet" href="media/css/thickbox.css" type="text/css" media="screen" /> -->
-<link rel="stylesheet" type="text/css" media="screen" href="media/css/leftNav.css">
-
+ 	<link rel="stylesheet" type="text/css" media="screen" href="media/css/home.css"> 
+	<link rel="stylesheet" type="text/css" media="screen" href="media/css/leftNav.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="media/css/body.css">
     <link rel="stylesheet" type="text/css" media="screen" href="media/css/footer.css">
 	<link rel="stylesheet" type="text/css" media="screen" href="media/css/form.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="media/css/home.css"> 
-	<link rel="stylesheet" type="text/css" media="screen" href="media/css/body.css">
+    
+
 	
 	<link rel="stylesheet" type="text/css" href="media/css/jqcloud.css" />
    
@@ -27,24 +27,18 @@
 <body>
 	
 <div id='mainContainer'>
-	<div id="ccGloBnr" class="tabon">	
-
-			<div class="bnrGtr">
+	<div id='headerContainer'>	
         		<div class="logo">
-				<a href="http://www.blazingstardelivery.com"><img src="media/images/logo.png" width="42" height="55" alt="UPS" border="0"></a>		
-			</div>
-			</div>	
+					<a href="http://www.blazingstardelivery.com"><img src="media/images/logo.png" width="42" height="55" alt="UPS" border="0"></a>		
+				</div>	
 		
 		<!-- begin primary navigation -->
-		<div class="taboff" id="ccGloBnr">
-					<div class="bnrGtr2">
 							<div class="navHeader">
 								<h1>
 									Sheffield Couriers
 								</h1>
 							</div>
-					</div>
-			<div class="nav1GloBnr">
+			<div class="nav1Bar">
 							<ul id="nav1">
 									<li class="tab-off" id="navList0">
 
@@ -71,16 +65,15 @@
 		 							</li>
 							</ul>
 			</div>
-		</div>
+		
 	</div>
 	<div style="clear:both"></div>
 				
 	<!-- end primary navigation -->
 
 		
-		
-<div class="bnrGtr2">		
-	<div class="navSearch" id="sec_nav_wrap">  
+				
+	<div class="navSearch">  
 		<form name="searchForm" action="/login">
 			<input type="hidden" name="loc" value="en_GB">
 			<input type="hidden" name="results" value="25">
@@ -90,15 +83,12 @@
 		</dl>
 		</form>
 	</div>
-</div>
-<script language="JavaScript1.2" src="/javascript/chng_lang.js?V=0708" type="text/javascript"></script>
 
-<div class="navLogin" id="usb_login">
+<div class="navLogin">
   <dl>
   	<dd><a href="#">New User</a><img alt="" border="0" height="1" src="media/images/1.gif" width="3"></dd>
   	<dd>|</dd>
 	<dd><a class="login" id="usb_login_link" href="#">Log-In</a><img alt="" border="0" height="1" src="media/images/1.gif" width="3"></dd>
-
   </dl>
 </div>
 
